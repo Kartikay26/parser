6f31efd4cdc5b606c252afaa6b3fbeb1b32f84b9
Kumar Karikay <krkartikay@gmail.com>
2019-10-26 01:08:48 +0530

    canonical sets contruction, some bugs remain

--- lr0.py
+++ lr0.py
@@ -64,9 +64,9 @@ class Grammar():
 			self.productionsMap[p.sym].append(p)
 		for p in self.productionsList[1:]:
 			for s in [p.sym] + p.prod:
-				if s.isTerminal:
+				if s.isTerminal and s not in self.terminals:
 					self.terminals.append(s)
-				else:
+				if s.isNonTerminal and s not in self.nonTerminals:
 					self.nonTerminals.append(s)
 	
 	def __repr__(self):
@@ -83,6 +83,8 @@ class LR0_Item():
 		self.dotAtEnd = (self.dot == len(self.p.prod))
 		if not self.dotAtEnd:
 			self.next_sym = self.p.prod[self.dot]
+		else:
+			self.next_sym = None
 
 	def __repr__(self):
 		s = f"{self.p}"
@@ -98,16 +100,14 @@ class LR0_Item():
 
 class LR0_ItemSet():
 	"""LR0_ItemSet: set of LR0_Items"""
-	item_sets_produced = 0
 
 	def __init__(self, items, g: Grammar):
 		"""items: list of item to construct item set from"""
 		self.items = items
 		self.grammar = g
+		self.state_number = None
 		self.apply_closure()
-		self.item_set_number = LR0_ItemSet.item_sets_produced
-		LR0_ItemSet.item_sets_produced += 1
-
+		
 	def apply_closure(self):
 		done = False
 		while not done:
@@ -121,16 +121,19 @@ class LR0_ItemSet():
 			done = True
 
 	def __repr__(self):
-		s = f"<ItemSet #{self.item_set_number}>\n"
+		if self.state_number is not None:
+			s = f"<ItemSet #{self.state_number}>\n"
+		else:
+			s = f"<ItemSet>\n"
 		for i in self.items:
 			s += f"\t{i}\n"
 		return s
 
 	def __hash__(self):
-		return hash(str(self))
+		return hash(self.items)
 
 	def __eq__(self, other):
-		return str(self) == str(other)
+		return self.items == other.items
 
 def apply_symbol(itemset: LR0_ItemSet, sym: Symbol) -> LR0_ItemSet:
 	new_item_list = []
@@ -138,19 +141,68 @@ def apply_symbol(itemset: LR0_ItemSet, sym: Symbol) -> LR0_ItemSet:
 		if not item.dotAtEnd and item.next_sym == sym:
 			new_item = LR0_Item(item.p, item.dot+1)
 			new_item_list.append(new_item)
-	print(new_item_list)
 	return LR0_ItemSet(new_item_list, itemset.grammar)
+
+def construct_canonical_sets(g: Grammar):
+	initialItem = LR0_Item(g.productionsList[0], 0)
+	i0 = LR0_ItemSet([initialItem], g)
+	canonical_item_sets = []
+	new_sets = [i0]
+	while len(new_sets) > 0:
+		canonical_item_sets += new_sets
+		freshly_added = []
+		for iset in new_sets:
+			applicable_symbols = []
+			for item in iset.items:
+				if item.next_sym:
+					applicable_symbols += [item.next_sym]
+			for sym in applicable_symbols:
+				iset_new = apply_symbol(iset, sym)
+				if (len(iset_new.items) > 0 and
+						iset_new not in canonical_item_sets):
+					freshly_added += [iset_new]
+		new_sets = freshly_added
+	for i, iset in enumerate(canonical_item_sets):
+		iset.state_number = i
+	return canonical_item_sets
+
+def construct_goto(g: Grammar, canonical_sets):
+	gotos = {}
+	for i in range(len(canonical_sets)):
+		for sym in g.nonTerminals:
+			new_set = apply_symbol(canonical_sets[i], sym)
+			if len(new_set.items) == 0:
+				gotos[(i, sym)] = -1 # error
+			else:
+				j = canonical_sets.index(new_set)
+				gotos[(i, sym)] = j
+	return gotos
+
+def construct_actions(g: Grammar, canonical_sets):
+	actions = {}
+	for i in range(len(canonical_sets)):
+		for sym in g.terminals:
+			new_set = apply_symbol(canonical_sets[i], sym)
+			if len(new_set.items) == 0:
+				actions[(i, sym)] = -1 # error
+			elif any(item.dotAtEnd for item in new_set.items):
+				actions[(i, sym)] = ("reduce", "?")
+			else:
+				j = canonical_sets.index(new_set)
+				actions[(i, sym)] = ("shift", j)
+	return actions
 
 def main():
 	g = Grammar("grammar.txt", "S")
 	print(g)
-	initialItem = LR0_Item(g.productionsList[0], 0)
-	i0 = LR0_ItemSet([initialItem], g)
-	print(i0)
-	i1 = apply_symbol(i0, Symbol("B"))
-	print(i1)
-	i2 = apply_symbol(i1, Symbol("B"))
-	print(i2)
+	canonical_sets = construct_canonical_sets(g)
+	gotos = construct_goto(g, canonical_sets)
+	actions = construct_actions(g, canonical_sets)
+	print("Goto:")
+	pprint(gotos)
+	print()
+	print("Actions:")
+	pprint(actions)
 
 if __name__ == '__main__':
 	main()
